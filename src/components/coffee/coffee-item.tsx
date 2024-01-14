3432b57acf8c31e0e3a79b599f1a9bb185189ecb
= <=>
2024-01-14 00:50:17 -0500

    Refactor CoffeeItem component and update CoffeeList component

--- src/components/coffee/coffee-item.tsx
+++ src/components/coffee/coffee-item.tsx
@@ -1,5 +1,9 @@
-const CoffeeItem = (key: string) => {
-  // console.log(coffee);
+type Props = {
+  key: number;
+  coffee: Coffee;
+};
+
+const CoffeeItem: React.FC<Props> = ({ key, coffee }) => {
   return (
     <div
       key={key}
@@ -9,23 +13,19 @@ const CoffeeItem = (key: string) => {
       <div className="coffeePopular absolute top-3 left-3 bg-[#F6C768] text-[#111315] px-2 py-1 rounded-xl smallText m-1">
         Popular
       </div>
-      {/* <img
-        className="coffeeImage rounded-xl pb-2"
-        src={coffee.coffee.image}
-        alt=""
-      /> */}
+      <img className="coffeeImage rounded-xl pb-2" src={coffee.image} alt="" />
 
       <div className="flex justify-between">
         <div className="coffeeTitle">House Coffee</div>
         <div className="coffeePrice priceText bg-[#BEE3CC] text-[#111315] px-3 py-1 rounded">
-          {/* {foo} */}
+          {coffee.price}
         </div>
       </div>
       <div className="flex">
         <img src="./images/Star_fill.svg" alt="" className="pl-1" />
-        {/* <div className="coffeeRating pl-1">{coffee.coffee.rating}</div> */}
-        <div className="coffeeVotes smallText pl-1">
-          {/* ({coffee.coffee.votes} votes) */}
+        <div className="coffeeRating pl-1">{coffee.rating}</div>
+        <div className="coffeeVotes smallText pl-1 items-center flex">
+          ({coffee.votes} votes)
         </div>
       </div>
     </div>
